24121dbcfd81985de3677b30670fcb836fac1035
Agustin Sarasua <64442857+a-sarasua@users.noreply.github.com>
2020-07-20 21:56:54 -0300

    API incluida

--- Script/main.js
+++ Script/main.js
@@ -1,5 +1,10 @@
 window.onload = function () {
+
+	const http = new XMLHttpRequest()
+
 	const sky = document.querySelectorAll('.sky,.skyBack')
+	const rainDrop = document.querySelectorAll('.rainDrop')
+	const snowFlake = document.querySelectorAll('.snowFlake')
 	const sun = document.querySelector('.sun')
 	const moon = document.querySelector('.moon')
 	const mercury = document.querySelector('.mercury')
@@ -10,17 +15,30 @@ window.onload = function () {
 	const gardenGrass = document.querySelector('.gardenGrass')
 	const normalClouds = document.querySelector('.normalClouds')
 	const rainClouds = document.querySelector('.rainClouds')
-	const rainDrop = document.querySelectorAll('.rainDrop')
-	const snowFlake = document.querySelectorAll('.snowFlake')
 	const plant1 = document.querySelector('.plant1')
+	const plant5 = document.querySelector('.plant5')
 	const plant7 = document.querySelector('.plant7')
 
 	let summer = true
 	let winter = false
+	let humidity = 50
+	let temperature = 25
+
+	function selectAction() {
+		if (temperature < 16){
+			if(!winter)
+			summer = false
+			summerToWinter()
+			if (humidity >= 75) setTimeout(winterToSnow,48000)
+		} else if(!summer) {
+			winter = false
+			winterToSummer()
+			if (humidity > 75) setTimeout(summerToRain, 48000)
+		}
+	}
 
 	function summerToWinter() {
 		winter = true
-		console.log('summer to winter')
 		sky[0].classList.remove('summerSkyAnimation')
 		sky[1].classList.remove('summerSkyAnimation')
 		sky[0].classList.add('winterSkyAnimation')
@@ -30,19 +48,23 @@ window.onload = function () {
 		moon.classList.remove('summerMoonAnimation')
 		moon.classList.add('winterMoonAnimation')
 		mercury.classList.remove('summerMercuryAnimation')
-		mercury.classList.add('winterMercuryAnimation')
+		mercury.classList.add('mercurySummerToWinter')
+		setTimeout(function(){
+			mercury.classList.remove('mercurySummerToWinter')
+			mercury.classList.add('winterMercuryAnimation')
+		},500)
 		stars.classList.remove('summerStars')
 		stars.classList.add('winterStars')
 		plants.classList.remove('summerPlants')
 		plants.classList.add('winterPlants')
 		setTimeout(function(){
 			plant1.classList.add('plantHidden')
+			plant5.classList.add('plantHidden')
 			plant7.classList.add('plantHidden')
-		}, 20000)
+		},20000)
 	}
 
 	function winterToSummer() {
-		console.log('winter to summer')
 		summer = true
 		sky[0].classList.remove('winterSkyAnimation')
 		sky[1].classList.remove('winterSkyAnimation')
@@ -53,7 +75,11 @@ window.onload = function () {
 		moon.classList.remove('winterMoonAnimation')
 		moon.classList.add('summerMoonAnimation')
 		mercury.classList.remove('winterMercuryAnimation')
-		mercury.classList.add('summerMercuryAnimation')
+		mercury.classList.add('mercuryWinterToSummer')
+		setTimeout(function(){
+			mercury.classList.remove('mercuryWinterToSummer')
+			mercury.classList.add('summerMercuryAnimation')
+		},500)
 		stars.classList.remove('winterStars')
 		stars.classList.add('summerStars')
 		plants.classList.remove('winterPlants')
@@ -64,12 +90,12 @@ window.onload = function () {
 		gardenGrass.style.backgroundColor = 'green'
 		setTimeout(function(){
 			plant1.classList.remove('plantHidden')
+			plant5.classList.remove('plantHidden')
 			plant7.classList.remove('plantHidden')
-		}, 20000)
+		},20000)
 	}
 
 	function summerToRain() {
-		console.log('summer to rain')
 		normalClouds.classList.add('normalCloudsRain')
 		sky[0].classList.remove('summerSkyAnimation')
 		sky[1].classList.remove('summerSkyAnimation')
@@ -80,13 +106,10 @@ window.onload = function () {
 			rainDrop[i].classList.remove('rainDropHidden')
 			rainDrop[i].classList.add('rainDropAppear')
 		}
-		setTimeout(function(){
-			rainToSummer()
-		}, 96000)
+		setTimeout(rainToSummer,96000)
 	}
 
 	function rainToSummer() {
-		console.log('rain to summer')
 		normalClouds.classList.remove('normalCloudsRain')
 		sky[1].classList.remove('rainSkyAnimation')
 		sky[0].classList.remove('rainSkyAnimation')
@@ -100,7 +123,6 @@ window.onload = function () {
 	}
 
 	function winterToSnow() {
-		console.log('winter to snow')
 		normalClouds.classList.add('normalCloudsRain')
 		sky[0].classList.remove('winterSkyAnimation')
 		sky[1].classList.remove('winterSkyAnimation')
@@ -116,14 +138,11 @@ window.onload = function () {
 			forest.classList.add('winterForest')
 			groundBack.style.backgroundColor = 'white'
 			gardenGrass.style.backgroundColor = 'white'			
-		}, 30000)
-		setTimeout(function(){
-			snowToWinter()
-		}, 96000)
+		},30000)
+		setTimeout(snowToWinter,96000)
 	}
 
 	function snowToWinter() {
-		console.log('snow to winter')
 		normalClouds.classList.remove('normalCloudsRain')
 		sky[1].classList.remove('snowSkyAnimation')
 		sky[0].classList.remove('snowSkyAnimation')
@@ -136,40 +155,20 @@ window.onload = function () {
 		}
 	}
 
-/*
-	sun.addEventListener('animationiteration', function(){
-		if(summer) {
-			summer = false
-			summerToRain()
+	function getWeatherInfo() {
+		http.open('GET', 'http://api.weatherapi.com/v1/current.json?key=de67ace33292425db51201946202007&q=buenos aires')
+		http.send()
+		http.onreadystatechange = function() {
+			if (http.responseText === '') return
+			const respuesta = JSON.parse(http.responseText)
+			humidity = respuesta.current.humidity
+			temperature = respuesta.current.temp_c
 		}
-	})*/
+	}
 
-	sun.addEventListener('animationiteration', function(){
-		if (summer){
-			summer = false
-			summerToWinter()
-		} else {
-			winter = false
-			winterToSnow()
-		}
-	})
+	sun.addEventListener('animationiteration', selectAction)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
+	getWeatherInfo()
+	setInterval(getWeatherInfo,96000)
 }
+
